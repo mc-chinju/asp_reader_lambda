dc04abf1e5dc5aa2e95392e1f1e5d1527d2d14a5
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2018-07-05 12:09:06 +0900

    A8にログイン

--- main.py
+++ main.py
@@ -5,28 +5,39 @@ import mechanize
 # Scraping Settings
 agent = mechanize.Browser()
 agent.addheaders = [("User-agent", "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4b) Gecko/20030516 Mozilla Firebird/0.6")]
+agent.set_handle_robots(False)
 with open("asp.yml", "r") as file:
     asp_info = yaml.safe_load(file.read())
 
 # Securities settings
 asp_names = list()
 with open("security.yml", "r") as file:
-    securities = yaml.safe_load(file.read())
+    security_info = yaml.safe_load(file.read())
+    # TODO: refactoring code!
     # Select target asps
-    for asp_name in securities.keys():
-        if securities[asp_name]["id"]:
+    for asp_name in security_info.keys():
+        if security_info[asp_name]["id"]:
             asp_names.append(asp_name)
 
 for asp_name in asp_names:
     login_page = asp_info[asp_name]["login"]
     data_page = asp_info[asp_name]["data"]
-
-    login_id = securities[asp_name]["id"]
-    password = securities[asp_name]["password"]
-
-    print(f"{affiliate} のデータ検索を開始します")
+    login_id = security_info[asp_name]["id"]
+    password = security_info[asp_name]["password"]
 
     agent.open(login_page)
+
+    if asp_name == "a8":
+        agent.select_form(name="asLogin")
+        agent["login"] = login_id
+        agent["passwd"] = password
+        agent.submit()
+        print(res.read())
+
+    elif asp_name == "felmat":
+    elif asp_name == "access_trade":
+    elif asp_name == "mosimo":
+    elif asp_name == "rentracks":
 
 # LINE Notify settings
 with open("line_notify.yml", "r") as file:
